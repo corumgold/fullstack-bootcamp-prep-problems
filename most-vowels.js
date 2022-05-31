db7965f95a3359459eca6570d30b0890f7c4f828
Corum Gold <goldcorum@gmail.com>
2022-05-31 17:01:44 -0400

    Return the word with the most vowels

--- most-vowels.js
+++ most-vowels.js
@@ -2,6 +2,7 @@ function mostVowels(string) {
     debugger;
     let words = string.split(' ');
     let mostVowels = ''
+    let mostVowelsCount = 0;
     for (let i = 0; i < words.length; i++) { //look through all words in the string
         let vowelCount = 0;
         for (let j = 0; j < words[i].length; j++) { //look through all letters in the word
@@ -13,7 +14,11 @@ function mostVowels(string) {
                 vowelCount++
             }
         }
-    }
-} 
+        if (vowelCount > mostVowelsCount) {
+            mostVowels = words[i];
+            mostVowelsCount = vowelCount;
+        }
+    } return mostVowels;
+}
 
 mostVowels("I love a parade")

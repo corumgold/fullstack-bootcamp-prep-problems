6980c33eac1da15c98c22f5bfc9d26165a098c13
Corum Gold <goldcorum@gmail.com>
2022-05-31 21:55:56 -0400

    Fix issue with noZeros concat

--- zero-dark-thirty.js
+++ zero-dark-thirty.js
@@ -1,14 +1,15 @@
 function zeroDarkThirty(num) {
+  debugger;
   if (num === 0) {
     return NaN;
   } else {
     let noZeros = ''
     let strNum = num.toString();
     for (let i = 0; i < strNum.length; i++)
-      if (strNum[i] !== 0) {
-        noZeros.concat(strNum[i])
-      }
-  }
+      if (strNum[i] !== '0') {
+        noZeros += strNum[i];
+      } return parseInt(noZeros);
+  } 
 }
 
-zeroDarkThirty(736)
+zeroDarkThirty(90.210);

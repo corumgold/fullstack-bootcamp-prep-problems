d3b32117d6a068093f5b75360ef4b205fd48fd51
Corum Gold <goldcorum@gmail.com>
2022-06-06 21:03:30 -0400

    Add starter file for zoo inventory exercise

---
+++ zoo_inventory.js
@@ -0,0 +1,17 @@
+// Define a function, zooInventory, that accepts a multi-dimensional array of animal facts.
+// zooInventory should return a new, flat array of strings.
+// Each element in the new array should be a sentence about each of the animals in the zoo.
+// Hint: nested for-loops are not necessary to complete this problem. 
+// This would be a great time to use template literals, also known as string interpolation.
+
+// let myZoo = [
+//   ['King Kong', ['gorilla', 42]],
+//   ['Nemo', ['fish', 5]],
+//   ['Punxsutawney Phil', ['groundhog', 11]]
+// ];
+
+// zooInventory(myZoo);
+// ['King Kong the gorilla is 42.',
+//        'Nemo the fish is 5.'
+//        'Punxsutawney Phil the groundhog is 11.']
+//

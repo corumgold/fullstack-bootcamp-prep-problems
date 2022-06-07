6b1e640ef278b76ac5972eb8033cdcb4f2a1b8df
Corum Gold <goldcorum@gmail.com>
2022-06-06 21:01:33 -0400

    Finish array flattener exercise

--- array_flattener.js
+++ array_flattener.js
@@ -1,3 +1,19 @@
 // Define a function, arrayFlattener, that accepts a 
 // two-dimensional array as an argument.
 // arrayFlattener should return a new, one-dimensional array.
+
+// arrayFlattener([1,[2, 3], 4]); // => [1, 2, 3, 4]
+
+function arrayFlattener(arr) {
+    let newArray = []
+    for (let i = 0; i < arr.length; i++) {
+        let element = arr[i]
+        if (Array.isArray(element)) {
+            for (let j = 0; j < element.length; j++) {
+                newArray.push(element[j]);
+            }
+        } else {
+            newArray.push(element)
+        }
+    } return newArray;
+}

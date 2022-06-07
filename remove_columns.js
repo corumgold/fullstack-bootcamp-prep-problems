491d9972868ca9f65c09ef3e8f90f5bb8a0640de
Corum Gold <goldcorum@gmail.com>
2022-06-06 21:41:58 -0400

    Add remove columns starter file

---
+++ remove_columns.js
@@ -0,0 +1,15 @@
+// Write a function removeColumns that accepts two arguments:
+
+// originalGrid (two-dimensional array)
+// numColums (number)
+// removeColumns should return a new grid with the correct number of columns removed.
+
+// removeColumns([[1, 2, 3],
+//                [1, 2, 3],
+//                [1, 2, 3],
+//                [1, 2, 3]], 2);
+// /* => [[1],
+//        [1],
+//        [1],
+//        [1]]
+// */

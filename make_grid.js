9729d422f9c70184a502575f2c6ee6e96dea51cc
Corum Gold <goldcorum@gmail.com>
2022-06-06 21:16:22 -0400

    Add make grid starter file

---
+++ make_grid.js
@@ -0,0 +1,13 @@
+// Write a function makeGrid that accepts two arguments:
+
+// numColumns (number) - how many columns we want our grid to have
+// numRows (number) - how many rows we want our grid to have
+
+// makeGrid should return a two-dimensional array that 
+// represents a grid of the given dimensions.
+
+// makeGrid(3,4);
+// [[1, 2, 3],
+// [1, 2, 3],
+// [1, 2, 3],
+// [1, 2, 3]]

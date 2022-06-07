92c511c7552bc43640dffb35033ca6c9f13f2d41
Corum Gold <goldcorum@gmail.com>
2022-06-06 21:34:48 -0400

    Finish make grid exercise

--- make_grid.js
+++ make_grid.js
@@ -11,3 +11,13 @@
 // [1, 2, 3],
 // [1, 2, 3],
 // [1, 2, 3]]
+
+function makeGrid(numColumns, numRows) {
+    let grid = [];
+    for (let i = 0; i < numRows; i++) {
+        let row = [];
+        for (let j = 1; j <= numColumns; j++) {
+            row.push(j);
+        } grid.push(row)
+    } return grid;
+}

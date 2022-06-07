4e84e2d94057edb6ba700903714cf4b7cab2007c
Corum Gold <goldcorum@gmail.com>
2022-06-07 17:24:38 -0400

    Create leet codex

--- leet_translator.js
+++ leet_translator.js
@@ -41,3 +41,11 @@ let letters = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
 let leetChars = ['@', '8', '(', '|)', '3', 'ph', 'g', '#','l', '_|', '|<', '1', "|'|'|", '/\/', '0', '|D', '(,)', '|2', '5', '+', '|_|', '|/', "|/|/'",'><', 'j', '2'];
 
 translate('Fullstack')    // => 'ph|_|115+@(|<'
+
+function leetTranslator(string){
+    debugger 
+    let leetCodex = {};
+     for (let i = 0; i < letters.length; i++){
+         leetCodex[letters[i]] = leetChars[i];
+     } return leetCodex
+} leetTranslator("heya")

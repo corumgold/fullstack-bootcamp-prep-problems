59531195073cd939523748d8d29a0b5fd69c3d37
Corum Gold <goldcorum@gmail.com>
2022-06-08 09:36:46 -0400

    Finish animal noises exercise

--- animal_noises.js
+++ animal_noises.js
@@ -8,40 +8,61 @@
 // petSounds('cat', 'Korea'); // => Cats in Korea say Nyaong!
 
 let animalNoises = [
-    { 'dog': {
-      'America' : 'Woof! Woof!',
-      'Germany' : 'Wau Wau!',
-      'England' : 'Bow wow!',
-      'Uruguay' : 'Jua jua!',
-      'Afrikaans' : 'Blaf!',
-      'Korea' : 'Mong mong!',
-      'Iceland' : 'Voff voff!',
-      'Albania' : 'Ham!',
-      'Algeria' : 'Ouaf ouaf!'
-      }
-    },
-    { 'cat': {
-      'America' : 'Meow',
-      'Germany' : 'Miauw!',
-      'England' : 'mew mew',
-      'Uruguay' : 'Miau Miau!',
-      'Afrikaans' : 'Purr',
-      'Korea' : 'Nyaong!',
-      'Iceland' : 'Kurnau!',
-      'Albania' : 'Miau',
-      'Algeria' : 'Miaou!'
-      }
-    },
-    { 'chicken': {
-      'America' : 'Cluck cluck',
-      'Germany' : 'tock tock tock',
-      'England' : 'Cluck Cluck',
-      'Uruguay' : 'gut gut gdak',
-      'Afrikaans' : 'kukeleku',
-      'Korea' : 'ko-ko-ko',
-      'Iceland' : 'Chuck-chuck!',
-      'Albania' : 'Kotkot',
-      'Algeria' : 'Cotcotcodet'
+  {
+    'dog': {
+      'America': 'Woof! Woof!',
+      'Germany': 'Wau Wau!',
+      'England': 'Bow wow!',
+      'Uruguay': 'Jua jua!',
+      'Afrikaans': 'Blaf!',
+      'Korea': 'Mong mong!',
+      'Iceland': 'Voff voff!',
+      'Albania': 'Ham!',
+      'Algeria': 'Ouaf ouaf!'
+    }
+  },
+  {
+    'cat': {
+      'America': 'Meow',
+      'Germany': 'Miauw!',
+      'England': 'mew mew',
+      'Uruguay': 'Miau Miau!',
+      'Afrikaans': 'Purr',
+      'Korea': 'Nyaong!',
+      'Iceland': 'Kurnau!',
+      'Albania': 'Miau',
+      'Algeria': 'Miaou!'
+    }
+  },
+  {
+    'chicken': {
+      'America': 'Cluck cluck',
+      'Germany': 'tock tock tock',
+      'England': 'Cluck Cluck',
+      'Uruguay': 'gut gut gdak',
+      'Afrikaans': 'kukeleku',
+      'Korea': 'ko-ko-ko',
+      'Iceland': 'Chuck-chuck!',
+      'Albania': 'Kotkot',
+      'Algeria': 'Cotcotcodet'
+    }
+  }
+];
+
+function petSounds(animal, country) {
+  debugger;
+  let noise;
+  let animalCappedPlural = animal.charAt(0).toUpperCase() + animal.slice(1) + "s"
+  for (let i = 0; i < animalNoises.length; i++) {
+    let currentAnimal = animalNoises[i];
+    for (let key in currentAnimal) {
+      if (key === animal) {
+        noise = animalNoises[i][key][country]
+      } else {
+        continue
       }
     }
-  ];
+  } return `${animalCappedPlural} in ${country} say ${noise}`;
+}
+
+petSounds('cat', "Germany")

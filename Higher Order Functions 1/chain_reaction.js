25b05ad7f59c26e0f114d0ead148f64f2a2b9139
Corum Gold <goldcorum@gmail.com>
2022-06-09 17:46:33 -0400

    finish chain reactin FIRST TRY

--- Higher Order Functions 1/chain_reaction.js
+++ Higher Order Functions 1/chain_reaction.js
@@ -24,3 +24,25 @@
 
 // chainReaction(0, [addTen, subtractFive, multiplyFive]); // => 25
 // chainReaction(0, [subtractFive, multiplyFive, addTen]); // => -15
+
+function chainReaction(initValue, funcsArray) {
+    debugger
+    let total = initValue;
+    funcsArray.forEach(func => {
+        total = func(total);
+    }); 
+    return total;
+}
+chainReaction(0, [addTen, subtractFive, multiplyFive]);
+
+function addTen(num) {
+  return num + 10;
+}
+
+function subtractFive(num) {
+  return num - 5;
+}
+
+function multiplyFive(num) {
+  return num * 5;
+}

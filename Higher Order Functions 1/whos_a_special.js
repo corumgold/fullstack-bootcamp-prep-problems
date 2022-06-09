153e03e03839ad534a532743dc6621f99cf4cc4e
Corum Gold <goldcorum@gmail.com>
2022-06-09 17:09:29 -0400

    Whos a special complete! FIRST TRY

--- Higher Order Functions 1/whos_a_special.js
+++ Higher Order Functions 1/whos_a_special.js
@@ -18,3 +18,26 @@
 // whosASpecial(specialPets);
 // // => Sadie the cat is very special! Layla the cat is very special! Bogie the dog is very special!
 
+function whosASpecial(array) {
+    debugger;
+    let specialString = ""
+    for (let i = 0; i < array.length; i++) {
+        let pet = array[i].name;
+        let type = array[i].species;
+        specialString += `${pet} the ${type} is very special! `
+    } return specialString.trim();
+}
+
+let specialPets = [{
+    name: 'Maevis',
+    species: 'cat'
+  }, {
+    name: 'Bella',
+    species: 'dog'
+  }, {
+    name: 'Meera',
+    species: 'dog'
+  }
+];
+
+whosASpecial(specialPets);

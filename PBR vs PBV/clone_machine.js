db914518c1f3e03c6bbd3ef2db3527792b7ff8f3
Corum Gold <goldcorum@gmail.com>
2022-06-09 11:38:26 -0400

    Add clone machine starter file

---
+++ PBR vs PBV/clone_machine.js
@@ -0,0 +1,16 @@
+// Write a function cloneMachine, that, given an argument animal (represented as an object), returns a clone of animal.
+
+// The name of the clone should be the name of its parent, concatenated with the word 'Clone'.
+
+// cloneMachine should also push the name of the clone to the parent's offspring array.
+
+// let dolly = {
+//   name: 'Dolly',
+//   species: 'sheep',
+//   offspring: []
+// }
+
+// let dollyClone = cloneMachine(dolly);
+
+// console.log(dollyClone) // {name: 'DollyClone', species: 'sheep', offspring: []}
+// console.log(dolly) // {name: 'Dolly', species: 'sheep', offspring: ['DollyClone']}

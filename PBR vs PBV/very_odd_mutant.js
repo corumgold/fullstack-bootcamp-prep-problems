57f8d7da0cae4826fcfef381adae70fc6d8c7da7
Corum Gold <goldcorum@gmail.com>
2022-06-09 11:33:47 -0400

    Add very odd mutant starter file

---
+++ PBR vs PBV/very_odd_mutant.js
@@ -0,0 +1,13 @@
+// Write a function, veryOddMutant, that accepts an array of numbers as an argument.
+// The function should mutate the given array by replacing every even number in the array with the string 'normie'.
+
+// veryOddMutant should return a count of the number of even numbers it replaced.
+
+// let allTheNums = [1, 2, 3, 4, 5, 6, 7, 8];
+// let countReplaced = veryOddMutant(allTheNums);
+
+// console.log('allTheNums:', allTheNums);
+// // [1, 'normie', 3, 'normie', 5, 'normie', 7, 'normie'];
+
+// console.log('countReplaced:', countReplaced);
+// // 4 (because 4 even numbers were replaced with 'normie')

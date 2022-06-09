6ade6a718d90f98f30f4c408e729dc165a2aeb9e
Corum Gold <goldcorum@gmail.com>
2022-06-09 16:34:45 -0400

    Add starter file for function logger exercise

---
+++ Higher Order Functions 1/function_logger.js
@@ -0,0 +1,28 @@
+// Define a function functionLogger that accepts 2 arguments:
+
+// 1) func (function) - a function to run
+
+// 2) arg (any type) - a value to pass into func as an argument
+
+// functionLogger should complete the following steps:
+
+// 1) log the message "Function starting"
+
+// 2) invoke the provided function
+
+// 3) log "Function complete"
+
+// 4) return the result of the function call
+
+// Note that the unit tests will check that the correct value is returned;
+// they do not check if you console logged the right values at the right time.
+
+// function squareNum(x) {
+//   return x * x;
+// }
+
+// let squareOfFour = functionLogger(squareNum, 4);
+// // Function starting
+// // Function complete
+
+// console.log(squareOfFour) // 16

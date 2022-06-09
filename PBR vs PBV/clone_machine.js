80e8bfe6ccb2770497d3f7898136a0440fc4a542
Corum Gold <goldcorum@gmail.com>
2022-06-09 12:40:24 -0400

    Finish clone machine exercise

--- PBR vs PBV/clone_machine.js
+++ PBR vs PBV/clone_machine.js
@@ -4,13 +4,25 @@
 
 // cloneMachine should also push the name of the clone to the parent's offspring array.
 
-// let dolly = {
-//   name: 'Dolly',
-//   species: 'sheep',
-//   offspring: []
-// }
 
-// let dollyClone = cloneMachine(dolly);
 
 // console.log(dollyClone) // {name: 'DollyClone', species: 'sheep', offspring: []}
 // console.log(dolly) // {name: 'Dolly', species: 'sheep', offspring: ['DollyClone']}
+
+function cloneMachine(animalObj) {
+    let clone = {
+        name: `${animalObj.name}Clone`,
+        species: animalObj.species,
+        offspring: []
+    }
+    animalObj.offspring.push(`${animalObj.name}Clone`)
+    return clone;
+}
+
+let dolly = {
+    name: 'Dolly',
+    species: 'sheep',
+    offspring: []
+}
+
+let dollyClone = cloneMachine(dolly);

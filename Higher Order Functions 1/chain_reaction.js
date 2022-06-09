0749967e7fd7924ef40fe69834f915b2b99edec0
Corum Gold <goldcorum@gmail.com>
2022-06-09 17:18:26 -0400

    Add starter file for chain reaction

---
+++ Higher Order Functions 1/chain_reaction.js
@@ -0,0 +1,26 @@
+// Define a function, chainReaction, that accepts 2 arguments:
+
+// 1) a starting value
+
+// 2) an array of functions
+
+// chainReaction should pass the starting value into the first function in the array. 
+// It should pass the value returned by the first function as an argument into the second function, 
+// and so on until every function in the array is called.
+
+// chainReaction should return the final value returned by the final function in the array.
+
+// function addTen(num) {
+//   return num + 10;
+// }
+
+// function subtractFive(num) {
+//   return num - 5;
+// }
+
+// function multiplyFive(num) {
+//   return num * 5;
+// }
+
+// chainReaction(0, [addTen, subtractFive, multiplyFive]); // => 25
+// chainReaction(0, [subtractFive, multiplyFive, addTen]); // => -15

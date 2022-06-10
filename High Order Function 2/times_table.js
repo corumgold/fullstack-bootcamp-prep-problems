3fd017e622433de19048e93303d00692d82c7841
Corum Gold <goldcorum@gmail.com>
2022-06-10 13:34:14 -0400

    Add times table starter file

---
+++ High Order Function 2/times_table.js
@@ -0,0 +1,11 @@
+// Define a function timesTable that accepts a number as an argument.
+
+// timesTable should generate a 'times table' function for any number passed in. 
+// The generated function should accept a number as an argument and return a function 
+// that itself returns the product of that number and any number passed in.
+
+// let ninesTable = timesTable(9);
+// ninesTable(8) // => 72
+
+// let twelvesTable = timesTable(12);
+// twelvesTable(100) // => 1200

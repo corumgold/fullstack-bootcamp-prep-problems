784a276d28709647807b26cd0ce82f70c872e599
Corum Gold <goldcorum@gmail.com>
2022-06-10 15:35:05 -0400

    Add backward string starter file

---
+++ Recursion 1/backward_string.js
@@ -0,0 +1,11 @@
+// Write a function, backwardString, that receives a string as an argument. 
+// It should log every letter in the string, from the last character to the first. Use recursion.
+
+// NOTE: As with the last problem, the tests will check how many times console.log is called.
+
+// backwardString('happy');
+// // y
+// // p
+// // p
+// // a
+// // h

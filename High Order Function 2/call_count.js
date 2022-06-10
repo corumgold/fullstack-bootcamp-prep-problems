9b51e8536880a0e74b65fc1d7fee5f2eb6ef3d07
Corum Gold <goldcorum@gmail.com>
2022-06-10 14:21:32 -0400

    Finish call count

--- High Order Function 2/call_count.js
+++ High Order Function 2/call_count.js
@@ -1,12 +1,21 @@
 // Write a function, callCount, that returns a new function. 
 // The new function should return the number of times its been called.
 
-// let newFunction1 = callCount();
-// let newFunction2 = callCount();
 
-// newFunction1(); // => 1
-// newFunction1(); // => 2
 
-// newFunction2(); // => 1
-// newFunction2(); // => 2
+function callCount () {
+    let counter = 0;
+    return function () {
+        counter++;
+        return counter;
+    }
+} 
 
+let newFunction1 = callCount();
+let newFunction2 = callCount();
+
+newFunction1(); // => 1
+newFunction1(); // => 2
+
+newFunction2(); // => 1
+newFunction2(); // => 2

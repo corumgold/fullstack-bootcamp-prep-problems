dc43459395497e56ef51671345488f7eea55f5f0
Corum Gold <goldcorum@gmail.com>
2022-06-10 14:27:33 -0400

    Finish stringify exercise

---
+++ High Order Function 2/stringify.js
@@ -0,0 +1,16 @@
+// Define a function, stringify, that takes a callback function as an argument and returns a new function. 
+// When the new function is called, it should invoke the callback function, coerce the value returned into a string, 
+// and then return that string.
+
+// function returnsANumber() {
+//   return 100;
+// }
+
+// let newFunction = stringify(returnsANumber);
+// newFunction(); // => '100'
+
+function stringify(func) {
+    return function () {
+        return func().toString(); 
+    }
+}

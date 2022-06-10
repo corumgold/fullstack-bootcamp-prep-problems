6d569bc5abc6fcd3dcd44ad3dae96ec45c739cc4
Corum Gold <goldcorum@gmail.com>
2022-06-10 13:54:01 -0400

    Finish partial exercise

---
+++ High Order Function 2/partial.js
@@ -0,0 +1,18 @@
+// Write a function, partial, that accepts a callback function and another value (let's call it argA) as arguments. 
+// partial should return a new function. When invoked, the new function will:
+
+// Accept its own argument (let's call it argB)
+// Call the callback, passing in argA as the first argument and argB as the second argument
+// Return the value returned by the callback
+// function summer (a, b) {
+//   return a + b
+// };
+
+// let sumFive = partial(summer, 5);
+// sumFive(10) // => 15;
+
+let partial = function (func, argA) {
+    return function(argB) {
+       return func(argA, argB);
+    } 
+}

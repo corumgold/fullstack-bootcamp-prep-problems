a2beb812ffeeec742d84770e2e59f812c6a32e12
Corum Gold <goldcorum@gmail.com>
2022-06-14 17:32:01 -0400

    Resolve even and odd

--- Arrays 2/even-and-odd.js
+++ Arrays 2/even-and-odd.js
@@ -19,3 +19,15 @@ evenAndOdd([1, 2, 3, 4, 5, 6]); // => [[ 2 ,4, 6 ], [ 1, 3, 5 ]]
 //     } evensAndOdds.push(evens, odds);
 //     return evensAndOdds;
 //   }
+
+function evenAndOdd(arr) {
+    let newArray = [[], []]
+    for (let i = 0; i < arr.length; i++) {
+        let num = arr[i];
+        if (num % 2 === 0) {
+            newArray[0].push(num);
+        } else {
+            newArray[1].push(num);
+        }
+    } return newArray
+}

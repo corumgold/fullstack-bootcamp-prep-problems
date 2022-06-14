27fbac52eaa7e92cb9f98739c53f82e4d532bc7b
Corum Gold <goldcorum@gmail.com>
2022-06-14 17:29:02 -0400

    Add instructions for even and odd

--- Arrays 2/even-and-odd.js
+++ Arrays 2/even-and-odd.js
@@ -1,13 +1,21 @@
-function evenAndOdd(arr){
-    let evens = [];
-    let odds = [];
-    let evensAndOdds = []
-    for (let i = 0; i < arr.length; i++){
-        if (arr[i]%2 === 0){
-            evens.push(arr[i])
-        } else {
-            odds.push(arr[i]);
-        }
-    } evensAndOdds.push(evens, odds);
-    return evensAndOdds;
-  }
+// Define a function, evenAndOdd, that accepts an array.
+
+// evenAndOdd should return a new array containing two smaller arrays. 
+// The first element in the new array should be an array with all of the even numbers from the original array. 
+// The second element in the new array should be an array with all of the odd numbers from the original array.
+
+evenAndOdd([1, 2, 3, 4, 5, 6]); // => [[ 2 ,4, 6 ], [ 1, 3, 5 ]]
+
+// function evenAndOdd(arr){
+//     let evens = [];
+//     let odds = [];
+//     let evensAndOdds = []
+//     for (let i = 0; i < arr.length; i++){
+//         if (arr[i]%2 === 0){
+//             evens.push(arr[i])
+//         } else {
+//             odds.push(arr[i]);
+//         }
+//     } evensAndOdds.push(evens, odds);
+//     return evensAndOdds;
+//   }

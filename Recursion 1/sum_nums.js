21cef26654c003c169c7a45ca8a166d0cd61daab
Corum Gold <goldcorum@gmail.com>
2022-06-15 11:55:58 -0400

    Finish sum nums (needed help)

--- Recursion 1/sum_nums.js
+++ Recursion 1/sum_nums.js
@@ -1,6 +1,13 @@
 // Write a function, sumNums, that takes a number, num, as an argument and returns the sum of all the numbers between 1 and num.
 // You can assume that num will be greater than 1. Use recursion.
 
-
+function sumNums(num) {
+    debugger;
+    if (num === 1) {
+        return 1;
+    }
+    let sum = num + sumNums(num - 1);
+    return sum;
+}
 
 sumNums(3); // => 6 (3 + 2 + 1)

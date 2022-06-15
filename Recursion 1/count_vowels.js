cc31914b27d6a2352cd902bd711a6dc1e692f000
Corum Gold <goldcorum@gmail.com>
2022-06-15 12:06:42 -0400

    Create base case

--- Recursion 1/count_vowels.js
+++ Recursion 1/count_vowels.js
@@ -1,7 +1,14 @@
 // Write a function, countVowels, that accepts a string as an argument and returns the number of
 // vowels in that string. Use recursion.
 
-
+function countVowels(string) {
+    debugger;
+    string.toLowerCase();
+    let vowels = ['a', 'e', 'i', 'o', 'u'];
+    if (!vowels.includes(string)) {
+        return 0;
+    }
+}
 
 
 countVowels('x'); // => 9

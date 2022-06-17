f3ccb8a8ad174e84904517171580dd19f7b6fc2a
Corum Gold <goldcorum@gmail.com>
2022-06-17 17:15:14 -0400

    Resolve is a vowel

--- Recursion 1/count_vowels.js
+++ Recursion 1/count_vowels.js
@@ -2,25 +2,41 @@
 // vowels in that string. Use recursion.
 
 let firstSolve
-function countVowels(string) {
-    debugger;
-    let lastChar = string[string.length - 1];
+// function countVowels(string) {
+//     debugger;
+//     let lastChar = string[string.length - 1];
 
-    if (string.length === 0) {
-        return 0;
-    }
+//     if (string.length === 0) {
+//         return 0;
+//     }
 
-    return ( isAVowel(lastChar) ? 1 : 0 ) + countVowels(string.slice(0, string.length - 1));
-};
+//     return ( isAVowel(lastChar) ? 1 : 0 ) + countVowels(string.slice(0, string.length - 1));
+// };
 
-function isAVowel(char) {
-    let vowels = ['a', 'e', 'i', 'o', 'u'];
-    return vowels.includes(char);
-}
+// function isAVowel(char) {
+//     let vowels = ['a', 'e', 'i', 'o', 'u'];
+//     return vowels.includes(char);
+// }
+
+let secondSolve
+// function countVowels(str) {
+//     debugger
+//     //basecase
+//     if (str.length === 0) return 0;
+
+//     let lastChar = str[str.length - 1];
+
+//     return (isAVowel(lastChar) ? 1 : 0) + countVowels(str.slice(0, str.length - 1))
+
+// }
 
 
+// function isAVowel(char) {
+//     return ("aeiouAEIOU".indexOf(char) != -1);
+// }
 
-countVowels('');
-countVowels('a'); // '' =>  0 'a' => 1
-countVowels('ab'); // 1 + 0 = 1
+
+// countVowels('');
+// countVowels('a'); // '' =>  0 'a' => 1
+// countVowels('ab'); // 1 + 0 = 1
 countVowels('aba'); // 1 + 1

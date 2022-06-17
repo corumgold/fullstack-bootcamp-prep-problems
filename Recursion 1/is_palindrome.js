73d7970322ca220f8c233d22ea95d21147330a8a
Corum Gold <goldcorum@gmail.com>
2022-06-17 16:49:46 -0400

    Finish is palindrome exercise

--- Recursion 1/is_palindrome.js
+++ Recursion 1/is_palindrome.js
@@ -4,9 +4,25 @@
 // Write a recursive function, isPalindrome, to check if a string is a palindrome. 
 // Return true if the string is a palindrome; otherwise, return false.
 
+function isPalindrome(str) {
+    debugger
+    let smallStr = str.toLowerCase();
+    let firstChar = smallStr[0];
+    let lastChar = smallStr[smallStr.length - 1]
+
+    if (smallStr.length === 1 || (smallStr.length === 2 && firstChar === lastChar)) return;
+
+    if (firstChar === lastChar) {
+        let choppedStr = smallStr.slice(1, smallStr.length - 1);
+        isPalindrome(choppedStr);
+    } else {
+        return false;
+    }
+    return true;
+}
 
 
 isPalindrome('Kayak'); // => true
-isPalindrome('NEVERODDOREVEN'); // => true
-isPalindrome('Tacocat'); // => true
-isPalindrome('SELFLESS'); // => false
+// isPalindrome('NEVERODDOREVEN'); // => true
+// isPalindrome('Tacocat'); // => true
+// isPalindrome('SELFLESS'); // => false

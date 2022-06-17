be367b0e872bf1327bbfa9c94d86419cce3e5e35
Corum Gold <goldcorum@gmail.com>
2022-06-17 14:58:21 -0400

    Finish sum digits exercise

--- Recursion 1/sum_digits.js
+++ Recursion 1/sum_digits.js
@@ -1,6 +1,12 @@
 // Write a function, sumDigits, that sums up all the digits of a number passed in as an argument.
 // You can assume that the argument will be a positive integer. Use recursion.
 
+function sumDigits(digits) {
+    let digitsString = digits.toString();
+    let lastNum = parseInt(digitsString[digitsString.length - 1]);
+    if (digits < 10) return digits;
 
+    return lastNum += sumDigits(Math.floor(digits / 10));
+}
 
-sumDigits(1234) // => 10        
+console.log(sumDigits(1234)) // => 10   

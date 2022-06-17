d20723bb4adfe64856b03d8bda5c8f5ce451c011
Corum Gold <goldcorum@gmail.com>
2022-06-17 16:30:17 -0400

    Add comments to sum digits

--- Recursion 1/sum_digits.js
+++ Recursion 1/sum_digits.js
@@ -2,11 +2,16 @@
 // You can assume that the argument will be a positive integer. Use recursion.
 
 function sumDigits(digits) {
-    let digitsString = digits.toString();
+    //change numbers into string
+    let digitsString = digits.toString(); 
+    //get last digit of string AS A NUMBER
     let lastNum = parseInt(digitsString[digitsString.length - 1]);
+    
+    //base case -- if there is only one number in digits, return it!
     if (digits < 10) return digits;
 
-    return lastNum += sumDigits(Math.floor(digits / 10));
+
+    return lastNum + sumDigits(Math.floor(digits / 10));
 }
 
 console.log(sumDigits(1234)) // => 10   

0015e4344a83dfd4211739e1954d60e7720a4e3c
Corum Gold <goldcorum@gmail.com>
2022-06-17 16:36:09 -0400

    Add is palindrome starter file

---
+++ Recursion 1/is_palindrome.js
@@ -0,0 +1,12 @@
+// A palindrome is a word that is spelled the same forwards and backwards. 
+// For example, "LEVEL", "RACECAR", and "KAYAK" are all palindromes, while "MOTOR", "RUDDER", and "DOGGED" are not palidromes.
+
+// Write a recursive function, isPalindrome, to check if a string is a palindrome. 
+// Return true if the string is a palindrome; otherwise, return false.
+
+
+
+isPalindrome('Kayak'); // => true
+isPalindrome('NEVERODDOREVEN'); // => true
+isPalindrome('Tacocat'); // => true
+isPalindrome('SELFLESS'); // => false

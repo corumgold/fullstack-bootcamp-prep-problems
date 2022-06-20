79a40285fb96c3b3378bc6c5d73f1775327b58d9
Corum Gold <goldcorum@gmail.com>
2022-06-20 18:40:14 -0400

    Solve array sum

--- 12 Recursion 2/01_array_sum.js
+++ 12 Recursion 2/01_array_sum.js
@@ -1,5 +1,17 @@
 // Write a function, arraySum, that accepts an array of numbers as an argument and returns the sum
 // of all the numbers in the array (no matter how nested!).
 
+let solveCount = 0;
+
+function arraySum(arr) {
+    debugger;
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        let ele = arr[i];
+        if (Array.isArray(ele)) {
+            sum += arraySum(ele);
+        } else sum += ele;
+    } return sum;
+}
 
 arraySum([1, [2, 3, [4]]]) // => 10

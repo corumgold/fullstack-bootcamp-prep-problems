b698dadaa8f5c89a2f70b93e5fe55cdcb672f21a
Corum Gold <goldcorum@gmail.com>
2022-06-20 18:30:01 -0400

    Add starter file for array sum

---
+++ 12 Recursion 2/01_array_sum.js
@@ -0,0 +1,5 @@
+// Write a function, arraySum, that accepts an array of numbers as an argument and returns the sum
+// of all the numbers in the array (no matter how nested!).
+
+
+arraySum([1, [2, 3, [4]]]) // => 10

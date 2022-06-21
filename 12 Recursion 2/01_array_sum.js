5058a6263c1ef69e40c0fff375c1072e07ec283b
Corum Gold <goldcorum@gmail.com>
2022-06-21 11:54:40 -0400

    Solve array sum (2nd)

--- 12 Recursion 2/01_array_sum.js
+++ 12 Recursion 2/01_array_sum.js
@@ -1,15 +1,17 @@
 // Write a function, arraySum, that accepts an array of numbers as an argument and returns the sum
 // of all the numbers in the array (no matter how nested!).
 
-let solveCount = 1;
+let solveCount = 2;
 
 function arraySum(arr) {
     let sum = 0;
     for (let i = 0; i < arr.length; i++) {
-        let ele = arr[i];
-        if (Array.isArray(ele)) {
-            sum += arraySum(ele);
-        } else sum += ele;
+        let element = arr[i];
+        if (Array.isArray(element)) {
+            sum += arraySum(element);
+        } else {
+            sum += element;
+        }
     } return sum;
 }
 
--- 12 Recursion 2/05_search_party.js
+++ 12 Recursion 2/05_search_party.js
@@ -10,16 +10,64 @@
 // Return null if you can't find name.
 
 const searchParty = (string, obj) => {
-  
+  debugger;
+  for (let place in obj) {
+    if (typeof obj[string] === 'string') {
+      let obj[string] = ;
+    }
+    if (Array.isArray(place)) {
+      for (let i = 0; i < place.length; i++) {
+        let element = place[i];
+        if (element === string) {
+          return [key, place]
+        }
+      }
+    }
+    if (typeof place === 'object') {
+      return searchParty(string, place);
+    } else if (place === string) {
+      return [obj, key];
+    }
+  }
 }
 
-let world = {
-  'Fullstack': {
-    '11th floor': 'Marge',
-    '25th floor': 'Francis'
+let newYorkCity = {
+  'Manhattan': {
+    'Uptown': {
+      'Washington Heights': 'Daniel',
+      'UWS': 'Cathy'
+    },
+    'Midtown': {
+      'Madison Square': 'Susan',
+      'Theater District': ['Robert', 'Latisha']
+    },
+    'Downtown': {
+      'Tribeca': 'Billy',
+      'Financial District': {
+        'Fullstack': {
+          '11th floor': ['David', 'Nimit'],
+          '25th floor': 'Ashi'
+        }
+      }
+    }
   },
-  'Subway': ['Jackie', 'Grumio']
+  'Brooklyn': {
+    'Bushwick': 'Marilyn',
+    'Bed-Stuy': ['Juan', 'Denice']
+  },
+  'Queens': {
+    'Astoria': 'Ella',
+    'Flushing': 'Eric'
+  },
+  'Bronx': {
+    'Fordham': 'Aaron',
+    'Melrose': 'Krysten'
+  },
+  'Staten Island': {
+    'Arlington': ['Nadine', 'Mose'],
+    'Elm Park': 'Arthur'
+  }
 };
 
-searchParty('Francis', world); // => ['Fullstack', '25th floor']
-searchParty('Franco', world); // => null
+searchParty('Susan', newYorkCity); // => ['Fullstack', '25th floor']
+// searchParty('Franco', world); // => null

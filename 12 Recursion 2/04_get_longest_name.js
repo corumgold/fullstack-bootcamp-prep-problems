6781dd6f054d1bff8c60ccec8e47235fe6324dba
Corum Gold <goldcorum@gmail.com>
2022-06-20 21:27:32 -0400

    Add get longest name starter file

---
+++ 12 Recursion 2/04_get_longest_name.js
@@ -0,0 +1,18 @@
+// Write a function, getLongestName, that accepts an object as an argument.
+// The object represents a family tree. Return the longest name in the family.
+
+let family = {
+  'Beverly Marquez': {
+    'Nina Rhone': {
+      'William Rhodes': null,
+      'Paul Nell': null,
+      'Sir Paddington the Fourth, of the county Wilstonshire': null
+    }
+  }
+};
+
+const getLongestName = (obj) => {
+  
+}
+
+getLongestName(family); // => 'Sir Paddington the Fourth, of the county Wilstonshire'

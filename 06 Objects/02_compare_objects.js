1bf1e2d6ffeefa2e2de4e3680edb75101d2ab117
Corum Gold <goldcorum@gmail.com>
2022-06-22 13:13:12 -0400

    Solve compare objects -- 3rd

--- 06 Objects/02_compare_objects.js
+++ 06 Objects/02_compare_objects.js
@@ -6,29 +6,17 @@
 // compareObjects({ name: 'nick' }, { name: 'nick' }) // -> true
 
 // compareObjects({ name: 'zeke' }, { name: 'zeke', age: 12 }) // -> false
-let firstSolve
-// function compareObjects(obj1, obj2) {
-//     stringOne = ''
-//     stringTwo = ''
-//     for (key in obj1) {
-//         stringOne += `${key} is ${obj1[key]}`;
-//     }
-//     for (key in obj2) {
-//         stringTwo += `${key} is ${obj2[key]}`;
-//     } if (stringOne === stringTwo) {
-//         return true;
-//     } else {
-//         return false;
-//     }
-// } 
 
-let secondSolve
-// function compareObjects(obj1, obj2) {
-//     for (let key in obj1) {
-//         if (obj1[key] !== obj2[key]) {
-//             return false;
-//         } else if (Object.keys(obj1).length !== Object.keys(obj2).length) {
-//             return false;
-//         }
-//     } return true;
-// }
+let solveCount = 3;
+
+function compareObjects(arg1, arg2) {
+    for (let key in arg1) {
+        arg1[key];
+        if (arg1[key] !== arg2[key]) return false;
+    }
+    for (let key in arg2) {
+        arg1[key];
+        if (arg1[key] !== arg2[key]) return false;
+    } 
+    return true;
+}

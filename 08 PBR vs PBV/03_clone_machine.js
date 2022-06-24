e6d2399973ae96aeb5ec9b7baff4c2dde80b62c9
Corum Gold <goldcorum@gmail.com>
2022-06-24 16:05:04 -0400

    Solve clone machine -- 3rd

--- 08 PBR vs PBV/03_clone_machine.js
+++ 08 PBR vs PBV/03_clone_machine.js
@@ -4,36 +4,28 @@
 
 // cloneMachine should also push the name of the clone to the parent's offspring array.
 
-// let dolly = {
-//     name: 'Dolly',
-//     species: 'sheep',
-//     offspring: []
-// }
+
+
+let solveCount = 3;
+
+function cloneMachine(animalObj) {
+    let clone = {
+        name: animalObj.name + "Clone",
+        species: animalObj.species,
+        offspring: []
+    }
+    animalObj.offspring.push(clone.name);
+    return clone;
+}
+
+let dolly = {
+    name: 'Dolly',
+    species: 'sheep',
+    offspring: []
+}
 
 let dollyClone = cloneMachine(dolly);
 
 // console.log(dollyClone) // {name: 'DollyClone', species: 'sheep', offspring: []}
 // console.log(dolly) // {name: 'Dolly', species: 'sheep', offspring: ['DollyClone']}
-
-let firstSolve
-// function cloneMachine(animalObj) {
-//     let clone = {
-//         name: `${animalObj.name}Clone`,
-//         species: animalObj.species,
-//         offspring: []
-//     }
-//     animalObj.offspring.push(`${animalObj.name}Clone`)
-//     return clone;
-// }
-
-let secondSolve
-// function cloneMachine(animalObj) {
-//     let clone = {
-//         name: `${animalObj.name}Clone`,
-//         species: animalObj.species,
-//         offspring: []
-//     }
-//     animalObj.offspring.push(clone.name);
-//     return clone;
-// }
 

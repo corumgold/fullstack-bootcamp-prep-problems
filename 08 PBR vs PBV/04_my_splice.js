16aa71af287e8f75974f2895eac9d14f27dfa0f6
Corum Gold <goldcorum@gmail.com>
2022-07-04 14:20:37 -0400

    Solve the problem without passing specs? -- 2nd

--- 08 PBR vs PBV/04_my_splice.js
+++ 08 PBR vs PBV/04_my_splice.js
@@ -5,51 +5,29 @@
 
 // Do not use .splice in your function.
 
-// let myArray = [1, 2, 3];
+// solveCount = 2;
 
-// mySplice(myArray, 1, 1, 'apples')) // => [2]
-
-// console.log(myArray)    // [1,'apples', 3]
-
-function mySplice(array, start, deleteCount, itemToAdd) {
-    let splicedArray = [];
-    let item = itemToAdd;
-    let itemsDeleted = 0
-    for (let i = 0; i < array.length; i++) {
-        if (start === undefined && deleteCount === undefined && itemToAdd === undefined) {
-            splicedArray.push(array[i]);
-        } else if (deleteCount === undefined && itemToAdd === undefined) {
-            if (i >= start) {
-                splicedArray.push(array[i])
-            }
-        } else {
-            if (i < start - 1) {
-                continue;
-            }
-            else if (i >= start && itemsDeleted < deleteCount) {
-                itemsDeleted++;
-                continue
-            } else if (i >= start && item !== undefined) {
-                splicedArray.push(itemToAdd);
-                item = undefined;
-                splicedArray.push(array[i])
-            } else {
-                splicedArray.push(array[i]);
-            }
-        }
-    } let array = splicedArray;
-    return array;;
+function mySplice(arr, start, delCount, toAdd) {
+  debugger;
+  let deletedItem = arr[start];
+  let firstHalf = [];
+  let secondHalf = [];
+  for (let i = 0; i < start; i++) {
+    firstHalf.push(arr[i]);
+  }
+  if (toAdd) {
+    firstHalf.push(toAdd);
+  }
+  for (let i = start + delCount; i < arr.length; i++) {
+    secondHalf.push(arr[i]);
+  }
+  arr = firstHalf.concat(secondHalf);
+  return [deletedItem];
 }
 
-//MODIFY THE ORIGINAL ARRAY!
-// function mySplice(array, start, deleteCount, itemToAdd) {
-//     let deleted = 0
-//     if (start !== undefined) {
-//         for (let i = 0; i < array.length; i++) {
-//             if (i === start) {
-//                 array.shift(array[2]);
-//             }
-//         }
-//     } return array;
-// }
-mySplice(['food', 'trash', 'clothes'], 1, 1);
+let ourStuff = ["food", "trash", "clothes"];
+mySplice(ourStuff, 1, 1);
+
+// let myArray = [1, 2, 3];
+// mySplice(myArray, 1, 1, "apples"); // => [2]
+// console.log(myArray); // [1,'apples', 3]

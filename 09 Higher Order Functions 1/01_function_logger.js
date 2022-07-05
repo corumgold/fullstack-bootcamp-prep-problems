9a60b0f1fcbba38db7acd25b1ddb766e5b342f9d
Corum Gold <goldcorum@gmail.com>
2022-07-05 11:54:36 -0400

    Solve function logger -- 2nd

--- 09 Higher Order Functions 1/01_function_logger.js
+++ 09 Higher Order Functions 1/01_function_logger.js
@@ -1,35 +1,35 @@
+//Test specs: https://codepen.io/FullstackAcademy/pen/EEKNJr?editors=0010
+
 // Define a function functionLogger that accepts 2 arguments:
 
 // 1) func (function) - a function to run
-
 // 2) arg (any type) - a value to pass into func as an argument
 
 // functionLogger should complete the following steps:
 
 // 1) log the message "Function starting"
-
 // 2) invoke the provided function
-
 // 3) log "Function complete"
-
 // 4) return the result of the function call
 
 // Note that the unit tests will check that the correct value is returned;
 // they do not check if you console logged the right values at the right time.
 
-// function squareNum(x) {
-//   return x * x;
-// }
-
-// let squareOfFour = functionLogger(squareNum, 4);
-// // Function starting
-// // Function complete
-
-// console.log(squareOfFour) // 16
-
-let functionLogger = function (func, arg) {
-    console.log("Function Starting");
-    func(arg);
-    console.log("Function Complete");
-    return func(arg);
+function squareNum(x) {
+  return x * x;
 }
+
+//solveCount = 2;
+
+function functionLogger(func, arg) {
+  console.log("Function starting");
+  let calledFunc = func(arg);
+  console.log("Function complete");
+  return calledFunc;
+}
+
+let squareOfFour = functionLogger(squareNum, 4);
+// Function starting
+// Function complete
+
+console.log(squareOfFour); // 16
---
+++ 09 Higher Order Functions 1/02_finder_function.js
@@ -0,0 +1,13 @@
+// Define a function finderFunction that accepts an array and a callback function as arguments.
+// finderFunction should pass each element from the array into the callback function.
+
+// If the callback returns true for any of the elements in the array, return the index of the current element.
+// If the callback never returns true, return -1;
+
+
+
+
+
+console.log(finderFunction(numbers, isEven)) // 3
+console.log(finderFunction(odds, isEven)) // -1
+

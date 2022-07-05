fce5a000f8334f4166557f8d03a8f96883f5cf6e
Corum Gold <goldcorum@gmail.com>
2022-07-05 13:54:30 -0400

    Finish whos a special -- 2nd

--- 09 Higher Order Functions 1/03_whos_a_special.js
+++ 09 Higher Order Functions 1/03_whos_a_special.js
@@ -1,42 +1,36 @@
+//Test specs: https://codepen.io/FullstackAcademy/pen/OvNWPp?editors=0010
+
 // Write a function whosASpecial that takes an array of pets as an argument and returns a string confirming what you already knew:
 //  all of the pets are very special! Feel free to add your own special pets to the array!
 
 // Use .forEach in your answer.
 
-// let specialPets = [{
-//     name: 'Sadie',
-//     species: 'cat'
-//   }, {
-//     name: 'Layla',
-//     species: 'cat'
-//   }, {
-//     name: 'Bogie',
-//     species: 'dog'
-//   }
-// ];
-
 // whosASpecial(specialPets);
 // // => Sadie the cat is very special! Layla the cat is very special! Bogie the dog is very special!
 
-function whosASpecial(array) {
-    let specialString = ""
-    for (let i = 0; i < array.length; i++) {
-        let pet = array[i].name;
-        let type = array[i].species;
-        specialString += `${pet} the ${type} is very special! `
-    } return specialString.trim();
-}
-
-let specialPets = [{
-    name: 'Maevis',
-    species: 'cat'
-  }, {
-    name: 'Bella',
-    species: 'dog'
-  }, {
-    name: 'Meera',
-    species: 'dog'
-  }
+let specialPets = [
+  {
+    name: "Maevis",
+    species: "cat",
+  },
+  {
+    name: "Bella",
+    species: "dog",
+  },
+  {
+    name: "Meera",
+    species: "dog",
+  },
 ];
+
+//solveCount = 2;
+
+function whosASpecial(arr) {
+  let specialString = "";
+  arr.forEach((element) => {
+    return specialString += ` ${element.name} the ${element.species} is very special!`;
+  });
+  return specialString.slice(1)
+}
 
 whosASpecial(specialPets);
